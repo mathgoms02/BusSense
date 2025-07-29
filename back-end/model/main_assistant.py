f779ed6ae433b5480bf9a3932441516ee0bf4e6b
Matheus Filipe Da Silva Gomes <matheusg@motorola.com>
2025-07-28 22:19:57 -0300

    feat: Added a host state changes: - Created a host state to direct the user to correct state - Updated model settings to better responses - Splited States in Classes

--- back-end/model/main_assistant.py
+++ back-end/model/main_assistant.py
@@ -9,11 +9,132 @@ from config import constants
 from utils import RouteEmbeddings, InteractionLogger
 
 #TODO:
-# - [ ] Refatorar para usar uma máquina de estados
+# - [X] Refatorar para usar uma máquina de estados
+# - [X] Criar um Anfitrião (AnfitriaoState) que vai classificar a intenção do usuário
 # - [X] Melhorar a lógica de feedback (retornar um "OK" ou "Obrigado" para o usuário)
-# - [ ] Estudar melhor ideia do Llama
-# - [ ] Mesclar script com o LlamaThinking
+# - [ ] Refatorar todo projeto, eliminando agentes e scripts desnecessários
 # - [ ] Criar Client para o Assistente para tools
+
+class State:
+    """ Classe base para estados do assistente. """
+    def __init__(self, assistant):
+        self.assistant = assistant
+
+    def handle_user_input(self, text: str):
+        """ Método que será implementado por cada estado para lidar com o input. """
+        raise NotImplementedError("Subclasses must implement this method.")
+
+
+class AnfitriaoState(State):
+    """
+    O estado central (Anfitrião). Ele é responsável por classificar
+    a intenção do usuário e decidir para qual estado ir.
+    """
+
+    def handle_user_input(self, text: str):
+        print("[AnfitriaoState]: Classificando intenção...")
+
+        intent = self.assistant._classify_intent_with_llm(text)
+        print(f"[AnfitriaoState]: Intenção classificada como: {intent}")
+
+        if intent == 'solicitar_rota':
+            print("[AnfitriaoState]: Intenção 'solicitar_rota' detectada. Transicionando.")
+            self.assistant.transition_to(RouteState(self.assistant))
+            self.assistant.state.handle_user_input(text)
+
+        elif intent == 'feedback':
+            print("[AnfitriaoState]: Intenção 'feedback' detectada. Transicionando.")
+            self.assistant.transition_to(FeedbackState(self.assistant))
+            self.assistant.state.handle_user_input(text)
+
+        elif intent == 'conversacao_geral':
+            print("[AnfitriaoState]: Intenção 'conversacao_geral' detectada.")
+
+            system_prompt = (
+                "Você é 'BusSense', um assistente de IA focado em transporte público. Sua personalidade é útil e profissional."
+                "Siga estas regras RIGOROSAMENTE:\n"
+                "1. Responda de forma direta e objetiva.\n"
+                "2. Mantenha as respostas curtas, com no máximo duas frases.\n"
+                "3. NUNCA use emojis, emoticons ou gírias.\n"
+                "4. NUNCA faça perguntas pessoais como 'Tudo bem com você?'.\n\n"
+                "Exemplo de resposta ideal para a entrada 'Olá':\n"
+                "Olá! Sou o BusSense, seu assistente de transporte. Como posso ajudar com sua rota hoje?"
+            )
+
+            messages = [
+                self.assistant.create_message(system_prompt, 'system'),
+                self.assistant.create_message(text, 'user')
+            ]
+
+            response = self.assistant._call_llm(messages, temperature=0.4, max_tokens=60)
+            print("Resposta do Assistente:", response)
+            self.assistant.tts.text = response
+            self.assistant.tts.convert_to_speech()
+
+        else: # intent == 'desconhecido'
+            print("[AnfitriaoState]: Não foi possível determinar a intenção.")
+            response = "Desculpe, não entendi. Pode repetir?"
+            print("Resposta do Assistente:", response)
+            self.assistant.tts.text = response
+            self.assistant.tts.convert_to_speech()
+
+
+class RouteState(State):
+    """ Estado para processar uma solicitação de rota. """
+    def handle_user_input(self, text: str):
+        print("[RouteState]: Processando solicitação de rota...")
+        log_data = {'user_query': text}
+
+        # Extrai origin e destination pela query
+        locations = self.assistant._extract_location_from_text(text)
+        # Filtra no banco pela origin e destination
+        filtered_data, _ = self.assistant.route_searcher.search(locations['origin'], locations['destination'])
+        # Gera o prompt para o modelo Llama
+        prompt = self.assistant._generate_route_prompt(filtered_data)
+
+        # Padroniza as mensagens para o LLM
+        messages = [
+            self.assistant.create_message(prompt, 'system'),
+            self.assistant.create_message(text, 'user')
+        ]
+        # Obtém a resposta do LLM
+        response_text = self.assistant._call_llm(messages)
+        print("Resposta do Assistente:", response_text)
+
+        self.assistant.tts.text = response_text
+        self.assistant.tts.convert_to_speech()
+
+        log_data['assistant_response'] = response_text
+        self.assistant.last_interaction_id = self.assistant.logger.log(log_data)
+
+        print("[RouteState]: Mudando para estado Anfitrião.")
+        self.assistant.transition_to(AnfitriaoState(self.assistant))
+
+
+class FeedbackState(State):
+    """ Estado para aguardar e processar o feedback do usuário."""
+    def handle_user_input(self, text: str):
+        print("[FeedbackState]: Processando feedback...")
+
+        # Usa o agente classificador para entender a intenção
+        intent_subtype = self.assistant.feedback_classifier._classify_intent(text)
+        print(f"Intenção detectada: {intent_subtype}")
+
+        if self.assistant.last_interaction_id:
+            self.assistant.feedback_classifier._handle_intent(self.assistant.last_interaction_id, intent_subtype, text)
+        else:
+            print("Registrando feedback geral do usuário.")
+            self.assistant.logger.log({'user_feedback_geral': text})
+
+        response = "Obrigado pelo seu feedback!"
+        print("Resposta do Assistente:", response)
+        self.assistant.tts.text = response
+        self.assistant.tts.convert_to_speech()
+
+        print("[FeedbackState]: Retornando ao estado anfitrião.")
+        self.assistant.transition_to(AnfitriaoState(self.assistant))
+
+# --- FIM DA MÁQUINA DE ESTADOS ---
 
 class BusSenseAssistant:
     def __init__(self, routes_data_path: str):
@@ -25,17 +146,17 @@ class BusSenseAssistant:
         self.logger = InteractionLogger()
         self.route_searcher = RouteEmbeddings(routes_df)
         self.feedback_classifier = FeedbackClassifierAgent()
-
-        # Interagindo com o modelo Llama via Requests
         self.llm_url = constants.LLAMA_API_URL
+        self.last_interaction_id = None
 
         # --- MÁQUINA DE ESTADOS ---
-        #TODO:  Adicionar um self.state inicial para controlar o fluxo
-        #       Estado esse que vai separar se é uma interação normal com o usuário, se é uma solicitação de rota ou se é um feedback.
-        self.state = "AGUARDANDO_ROTA"
-        self.last_interaction_id = None
-        print(f"Estado inicial: {self.state}")
+        self.state = AnfitriaoState(self)
+        print(f"Estado inicial: {self.state.__class__.__name__}")
 
+    def transition_to(self, new_state: State):
+        """ Método central para mudar de estado."""
+        self.state = new_state
+        print(f"Transição para o novo estado: {self.state.__class__.__name__}")
 
     def start(self):
             """ Inicia o loop de escuta do assistente. """
@@ -45,84 +166,7 @@ class BusSenseAssistant:
                 transcribed_text = self.recorder.listen()
 
                 if transcribed_text:
-                    self.handle_user_input(transcribed_text)
-
-
-    def handle_user_input(self, text: str):
-        """ Ponto de entrada central que decide o que fazer com base no estado. """
-        print(f"\n--- Processando Input no Estado: {self.state} ---")
-
-        #TODO: Adicionar modelo genérico de acordo com o self.state primário ou "Anfitrião"
-        if self.state == "AGUARDANDO_ROTA":
-            self._process_new_route_request(text)
-
-        elif self.state == "AGUARDANDO_FEEDBACK":
-            # Se acabamos de dar uma rota, o input é provavelmente um feedback.
-            self._process_potential_feedback(text)
-
-
-    def _process_new_route_request(self, text: str):
-        """ Lógica para lidar com uma nova solicitação de rota. """
-        print("Interpretando como uma nova solicitação de rota...")
-        log_data = {'user_query': text}
-
-        # Extrai origin e destination pela query
-        locations = self._extract_location_from_text(text)
-        # Filtra no banco pela origin e destination
-        filtered_data, _ = self.route_searcher.search(locations['origin'], locations['destination'])
-        # Gera o prompt para o modelo Llama
-        prompt = self._generate_route_prompt(filtered_data)
-
-        # Padroniza as mensagens para o LLM
-        system_message = self.create_message(prompt, 'system')
-        user_message = self.create_message(text, 'user')
-
-        # Obtém a resposta do LLM
-        response_text = self._call_llm(system_message, user_message)
-        print("Resposta do Assistente:", response_text)
-        self.tts.text = response_text
-        self.tts.convert_to_speech()
-
-        # Loga a interação
-        log_data['assistant_response'] = response_text
-        self.last_interaction_id = self.logger.log(log_data)
-
-        #TODO: Voltar para estado anfitrião
-        # *** MUDANÇA DE ESTADO "CRUCIAL" ***
-        self.state = "AGUARDANDO_FEEDBACK"
-        print(f"Estado alterado para: {self.state}. Aguardando feedback para a interação {self.last_interaction_id}.")
-
-
-    def create_message(self, message, role):
-            return {"role": role,
-                "content": message}
-
-
-    def _process_potential_feedback(self, text: str):
-        """ Lógica para lidar com um input que pode ser feedback. """
-        print("Interpretando como potencial feedback...")
-
-        # Usa o agente classificador para entender a intenção
-        intent = self.feedback_classifier._classify_intent(text) # Classifica por palavras-chave
-        print(f"Intenção detectada: {intent}")
-
-        if intent == "nova_solicitacao":
-            # O usuário ignorou o contexto de feedback e fez uma nova pergunta.
-            print("O usuário fez uma nova solicitação. Resetando o estado.")
-            self.state = "AGUARDANDO_ROTA"
-            self.handle_user_input(text)
-        else:
-            # É um feedback! Processa e volta ao estado inicial.
-            if self.last_interaction_id:
-                self.feedback_classifier._handle_intent(self.last_interaction_id, intent, text)
-
-            #TODO: Voltar para estado anfitrião
-            # *** MUDANÇA DE ESTADO "CRUCIAL" ***
-            self.state = "AGUARDANDO_ROTA"
-            self.tts.text = f"Feedback processado."
-            self.tts.convert_to_speech()
-            print(f"Feedback processado. Estado alterado para: {self.state}.")
-
+                    self.state.handle_user_input(transcribed_text)
 
     def _extract_location_from_text(self, text):
         """ Extrai a origem e o destino de uma frase do usuário usando o modelo Llama. """
@@ -130,12 +174,12 @@ class BusSenseAssistant:
             {
                 "role": "system",
                 "content": (
-                    "You are an assistant that extracts the city of origin and destination from a user's sentence. "
-                    "Return ONLY a valid JSON object in the following format:\n"
+                    "Você é um assistente que extrai a cidade de origem e destino da frase de um usuário. "
+                    "Retorne APENAS um objeto JSON válido no seguinte formato:\n"
                     '{ "origin": "CITY1", "destination": "CITY2" }\n'
-                    "If you can't find one, return:\n"
+                    "Se não conseguir encontrar, retorne:\n"
                     '{ "origin": "UNKNOWN", "destination": "UNKNOWN" }\n'
-                    "Do not write anything else. Do not tell stories. Do not explain."
+                    "Não escreva mais nada. Não conte histórias. Não explique."
                 )
             },
             {
@@ -162,7 +206,6 @@ class BusSenseAssistant:
 
         return locations
 
-
     def _generate_route_prompt(self, filtered_data):
         """ Gera o prompt para o modelo Llama com as rotas filtradas """
 
@@ -172,37 +215,87 @@ class BusSenseAssistant:
         json_data = filtered_data.to_json(orient='records')
 
         prompt = f"""
-            You are an expert public transport assistant. Based on the following bus routes in JSON format,
-            suggest the best one to the user. Your task is to identify the best route and respond with a concise sentence.
-            Use 'Linha' for the RouteCode, 'Ponto Inicial' for RouteStart, and 'Ponto Final' for RouteEnd.
-            For example: "A melhor rota é a Linha 708, que vai de Monte Mor até Campinas."
-            Do not include any backticks or additional explanations. Only return the final sentence.
+            Você é um assistente especialista em transporte público. Com base nas seguintes rotas de ônibus em formato JSON, 
+            sugira a melhor rota para o usuário. Sua tarefa é identificar a melhor rota e responder com uma frase concisa.
+            Use 'Linha' para o RouteCode, 'Ponto Inicial' para RouteStart, e 'Ponto Final' para RouteEnd.
+            Por exemplo: "A melhor rota é a Linha 708, que vai de Monte Mor até Campinas."
+            Não inclua acentos graves (`) ou explicações adicionais. Retorne apenas a frase final.
 
             {json_data}
         """
         return prompt
 
-
-    def _call_llm(self, prompt, user_text):
+    def _call_llm(self, messages: list, temperature: float = 0.7, max_tokens: int = 2048):
         """ Faz a chamada ao modelo Llama (via requests) com o prompt e o texto do usuário. """
         form = {
             "model": constants.LLAMA_MODEL_NAME,
-            "temperature": 0.7,
-            "max_tokens": 2048,
+            "temperature": temperature, # Quanto maior a temperatura, mais criativa a resposta (Ruim para classificação)
+            "max_tokens": max_tokens,
             "seed": 42,
-            "messages": [prompt, user_text],
+            "messages": messages,
             "repeat_last_n": 64,
             "repeat_penalty": 1.2
         }
+        try:
+            response = requests.post(self.llm_url, json=form)
+            json_list = response.json()
 
-        response = requests.post(self.llm_url, json=form)
-        json_list = response.json()
+            if 'error' in json_list:
+                print("Erro na requisição:", json_list['error']['message'])
+                return "Desculpe, não consegui entender a solicitação. Tente novamente com menos detalhes."
+            print("[DEBUG] Resposta do LLM:", json_list['choices'][0]['message']['content'])
+            return json_list['choices'][0]['message']['content']
+        except requests.exceptions.RequestException as e:
+            print(f"[ERROR] Erro ao chamar o LLM: {e}")
+            return "Desculpe, estou com problemas para me conectar ao meu cérebro. Tente novamente mais tarde."
 
-        if 'error' in json_list:
-            print("Erro na requisição:", json_list['error']['message'])
-            return "Desculpe, não consegui entender a solicitação. Tente novamente com menos detalhes."
+    def _classify_intent_with_llm(self, text: str) -> str:
+        """
+        Usa o LLM para classificar a intenção do Usuário.
+        Retorna uma string simples: 'solicitar_rota', 'feedback', 'conversa_geral' ou 'desconhecido'.
+        """
+        system_prompt = (
+            "Sua tarefa é classificar a intenção de uma frase do usuário para um assistente de transporte público. "
+            "Responda APENAS com uma das seguintes palavras: "
+            "Siga os exemplos abaixo:\n\n"
+            "Usuário: 'Olá, tudo bem?'\n"
+            "Assistente: conversacao_geral\n\n"
+            "Usuário: 'Qual o ônibus para o centro?'\n"
+            "Assistente: solicitar_rota\n\n"
+            "Usuário: 'Gostei muito do serviço, obrigado!'\n"
+            "Assistente: feedback\n\n"
+            "Usuário: 'O motorista não parou para mim.'\n"
+            "Assistente: feedback\n\n"
+            "Usuário: 'como eu chego na prefeitura?'\n"
+            "Assistente: solicitar_rota\n\n"
+            "Usuário: 'valeu'\n"
+            "Assistente: conversacao_geral\n\n"
+            "Agora, classifique a seguinte frase do usuário. Não escreva mais nada. Apenas a palavra de classificação."
+        )
 
-        return json_list['choices'][0]['message']['content']
+        messages = [
+            self.create_message(system_prompt, 'system'),
+            self.create_message(text, 'user')
+        ]
+
+        try:
+            # reutilizar a função _call_llm, mas talvez com parametros dieferentes no futuro
+            response = self._call_llm(messages, temperature=0.1, max_tokens=10)
+
+            cleaned_response = response.strip().lower().replace("'", "")
+            if cleaned_response in ['solicitar_rota', 'feedback', 'conversacao_geral']:
+                print(f"[DEBUG] Intenção classificada: {cleaned_response}")
+                return cleaned_response
+            else:
+                print(f"RAW Response: '{response}'")
+                return 'desconhecido'
+        except Exception as e:
+            print(f"[ERROR] Erro ao classificar intenção: {e}")
+            return 'desconhecido'
+
+    def create_message(self, message, role):
+            return {"role": role,
+                "content": message}
 
 
 if __name__ == "__main__":
